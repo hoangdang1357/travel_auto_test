c230c6c372c51c3870dbe6ceed5b91f87630519d
Dang Hoang <hdjhdbsudg@gmail.com>
2025-09-23 21:24:25 +0700

    Fix import path for SearchPage in test_search_hotel_flight.py

--- tests/selenium_test/test_search_hotel_flight.py
+++ tests/selenium_test/test_search_hotel_flight.py
@@ -1,7 +1,7 @@
 import csv
 import pytest
 from selenium import webdriver
-from tests.selenium_test.pages.SearchPage import SearchPage
+from pages.SearchPage import SearchPage
 import os
 
 # helper to load test data from CSV
